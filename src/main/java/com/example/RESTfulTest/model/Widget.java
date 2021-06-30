24eed4cd079aa98f82307738f121d886dd0bde8d
Cristian <camilo.santamaria322@gmail.com>
2021-06-29 21:16:02 -0500

    Se realiza PUT y GET

--- src/main/java/com/example/RESTfulTest/model/Widget.java
+++ src/main/java/com/example/RESTfulTest/model/Widget.java
@@ -29,6 +29,10 @@ public class Widget {
         this.version = version;
     }
 
+    public Widget(Long id){
+        this.id = id;
+    }
+
     public Long getId() {
         return id;
     }
--- src/test/java/com/example/RESTfulTest/controller/WidgetRestControllerTest.java
+++ src/test/java/com/example/RESTfulTest/controller/WidgetRestControllerTest.java
@@ -14,11 +14,13 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import javax.swing.text.html.Option;
 import java.util.Optional;
 
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.ArgumentMatchers.any;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -37,7 +39,7 @@ class WidgetRestControllerTest {
 
     @Test
     @DisplayName("GET /widgets success")
-    void testGetWidgetsSuccess() throws Exception {
+    void getExistoso() throws Exception {
         // Setup our mocked service
         Widget widget1 = new Widget(1l, "Widget Name", "Description", 1);
         Widget widget2 = new Widget(2l, "Widget 2 Name", "Description 2", 4);
@@ -79,6 +81,27 @@ class WidgetRestControllerTest {
     }
 
     @Test
+    @DisplayName("GET /rest/widget/1L -  Found")
+    void testGetWidgetByIdFound() throws Exception {
+        //Se usa optional porque puede que devuelva algun error
+        Optional <Widget> widget = Optional.of(new Widget(1l, "Widget Name", "Description", 1));
+        doReturn(widget).when(service).findById(1L);
+
+        // Execute the GET request
+        mockMvc.perform(get("/rest/widget/{id}", 1L))
+                // Validate the response code
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                //Se pone el header
+                .andExpect(header().string(HttpHeaders.LOCATION, "/rest/widget/1"))
+                //Traigo los datos
+                .andExpect(jsonPath("$.id", is(1)))
+                .andExpect(jsonPath("$.name", is("Widget Name")))
+                .andExpect(jsonPath("$.description", is("Description")))
+                .andExpect(jsonPath("$.version", is(1)));
+    }
+
+    @Test
     @DisplayName("POST /rest/widget")
     void testCreateWidget() throws Exception {
         // Setup our mocked service
@@ -97,7 +120,6 @@ class WidgetRestControllerTest {
 
                 // Validate headers
                 .andExpect(header().string(HttpHeaders.LOCATION, "/rest/widget/1"))
-                .andExpect(header().string(HttpHeaders.ETAG, "\"1\""))
 
                 // Validate the returned fields
                 .andExpect(jsonPath("$.id", is(1)))
@@ -105,6 +127,32 @@ class WidgetRestControllerTest {
                 .andExpect(jsonPath("$.description", is("This is my widget")))
                 .andExpect(jsonPath("$.version", is(1)));
     }
+
+    @Test
+    @DisplayName("PUT /rest/widget/{id}")
+    void testUpdateWidget() throws Exception {
+        Widget widgetUpdate = new Widget(1l,"Otro nombre", "Una descripcion mas grande", 1);
+        Optional<Widget> widgetOriginal = Optional.of(new Widget(1l, "Widget Name", "Description", 1));
+        //En este buscare por ID
+        doReturn(widgetOriginal).when(service).findById(1l);
+        //En este me base en el de POST
+        doReturn(widgetUpdate).when(service).save(any());
+
+        mockMvc.perform(put("/rest/widget/{id}",1l)
+                .content(asJsonString(widgetUpdate))
+                .contentType(MediaType.APPLICATION_JSON)
+                //Este sera el Header se usa con el match para comparar
+                .header(HttpHeaders.IF_MATCH,"1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(header().string(HttpHeaders.LOCATION, "/rest/widget/1"))
+                .andExpect(jsonPath("$.id", is(1)))
+                .andExpect(jsonPath("$.name", is("Widget Name 2")))
+                .andExpect(jsonPath("$.description", is("Description 2")))
+                .andExpect(jsonPath("$.version", is(1)));
+
+    }
+
 
 
     static String asJsonString(final Object obj) {
